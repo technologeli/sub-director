ccd16b021512431410a2f3bad65983ae141bb867
Elijah Lieu <elijahlieu@protonmail.com>
2022-07-05 11:09:39 -0400

    css changes on newsub.tsx and button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ type ButtonProps = React.ComponentProps<"button">;
 const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
   return (
     <button
-      className={`rounded bg-zinc-900 px-8 py-1 text-zinc-200 
+      className={`rounded bg-zinc-900 px-8 py-1 text-zinc-200
       transition-colors hover:bg-zinc-800 ${className}`}
       {...props}
     >
--- src/pages/newsub.tsx
+++ src/pages/newsub.tsx
@@ -31,31 +31,35 @@ const CreateSubDirectory: NextPage = () => {
 
   return (
     <Shell subtitle="Create Sub Directory">
-      <div className="px-4 py-4">
-        <Form
-          onSubmit={handleSubmit((d) => {
-            mutation.mutate({ name: d.name as string });
-          })}
-        >
-          <h3 className="pt-2 text-center text-xl font-semibold">
-            New Subdirectory
-          </h3>
-          <TextInput
-            label="Name"
-            aria-required
-            name="name"
-            register={register}
-            error={errors.name}
-          />
-          <Button
-            className="py-2 font-bold"
-            type="submit"
-            disabled={mutation.isLoading}
+      <div className="flex justify-center">
+        <div className="max-w-3xl flex-grow px-4 py-4">
+          <Form
+            onSubmit={handleSubmit((d) => {
+              mutation.mutate({ name: d.name as string });
+            })}
           >
-            Create
-          </Button>
-          {mutation.error && <ErrorText>{mutation.error.message}</ErrorText>}
-        </Form>
+            <h3 className="pt-2 text-center text-xl font-semibold">
+              New Subdirectory
+            </h3>
+            <TextInput
+              label="Name"
+              aria-required
+              name="name"
+              register={register}
+              error={errors.name}
+            />
+            <div className="flex justify-center">
+              <Button
+                className="flex-grow py-2 font-bold"
+                type="submit"
+                disabled={mutation.isLoading}
+              >
+                Create
+              </Button>
+            </div>
+            {mutation.error && <ErrorText>{mutation.error.message}</ErrorText>}
+          </Form>
+        </div>
       </div>
     </Shell>
   );
